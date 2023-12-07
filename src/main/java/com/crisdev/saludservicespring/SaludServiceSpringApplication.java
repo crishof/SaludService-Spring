2883ad7cece75a2fb93cb724df858ff78f295d61
crishof <cristianhof@gmail.com>
2023-12-08 00:19:36 +0100

    revert

---
+++ src/main/java/com/crisdev/saludservicespring/SaludServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.crisdev.saludservicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludServiceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludServiceSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crisdev/saludservicespring/SaludServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisdev.saludservicespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludServiceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
