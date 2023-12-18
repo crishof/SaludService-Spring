987d5e9bb606b43647f277dc320f6b90fcd401b2
crishof <cristianhof@gmail.com>
2023-12-18 18:32:19 +0100

    pagination

--- src/main/java/com/crisdev/saludservice/controller/TurnoController.java
+++ src/main/java/com/crisdev/saludservice/controller/TurnoController.java
@@ -4,10 +4,13 @@ import com.crisdev.saludservice.enums.DiaSemana;
 import com.crisdev.saludservice.exception.MiException;
 import com.crisdev.saludservice.model.HorarioLaboral;
 import com.crisdev.saludservice.model.Profesional;
+import com.crisdev.saludservice.model.Turno;
 import com.crisdev.saludservice.repository.ProfesionalRepository;
 import com.crisdev.saludservice.service.ProfesionalService;
 import com.crisdev.saludservice.service.TurnoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -27,5 +30,18 @@ public class TurnoController {
 
     @Autowired
     ProfesionalRepository profesionalRepository;
+
+    @GetMapping("/verTurnos")
+    public String verTurnos(ModelMap modelMap,
+                            @RequestParam(defaultValue = "0") int page,
+                            @RequestParam(defaultValue = "15") int size) {
+
+        PageRequest pageable = PageRequest.of(page, size);
+        Page<Turno> turnos = turnoService.listarTurnos(pageable);
+
+        modelMap.addAttribute("turnos", turnos);
+        return "turno_lista";
+
+    }
 
 }
