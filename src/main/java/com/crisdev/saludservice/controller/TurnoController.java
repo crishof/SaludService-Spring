de2bc426b6187706b2226b8fec760a17a40212e3
crishof <cristianhof@gmail.com>
2023-12-22 10:10:26 +0100

    valoracion final

--- src/main/java/com/crisdev/saludservice/controller/TurnoController.java
+++ src/main/java/com/crisdev/saludservice/controller/TurnoController.java
@@ -10,6 +10,7 @@ import com.crisdev.saludservice.service.TurnoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
